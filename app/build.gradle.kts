17f6ce65a7bdb35b83f514991505f22efd1e4c0f
sarkerramjan2015 <sarkerramjan2015@gmail.com>
2026-09-06 07:53:54 +0600

    chore: bump version to 1.7 (versionCode 11) and ignore local keystore credentials file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,8 +97,8 @@ android {
     applicationId = "com.batchfee.edu"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10
-    versionName = "1.6.3"
+    versionCode = 11
+    versionName = "1.7"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
